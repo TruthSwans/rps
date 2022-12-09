d650d8b9f92e3ffa234878b3d9670a06c72f1096
TruthSwans <truthswans@gmail.com>
2022-12-09 07:13:38 -0700

    Modify loop to prompt player choice each round

--- rps-game.js
+++ rps-game.js
@@ -1,38 +1,59 @@
-//Declare variable for rock, paper, scissors//
-let rps = ["Rock","Paper","Scissors",];
-//Declare variable to randomly choose one of the three//
-let rpsRandom = rps[Math.floor(Math.random() * rps.length)];
-//Declare variable to assign that random selection to the Computer choice to display on the page//
-const computerSelection = getComputerChoice();
-//Call a function that returns the random selection and assigns the selection as the Computer choice//
-function getComputerChoice (){
-    let computerRPS = rpsRandom.toLowerCase();
-    return computerRPS;
+let playerScore = 0
+let computerScore = 0
+let draws = 0
+let rounds = 0
+
+let computerSelection = getComputerChoice()
+
+let playerSelection = prompt("Rock, Paper, or Scissors?", "")
+
+function getComputerChoice() {
+    // your code here!
+    const choices = [`rock`, `paper`, `scissors`]
+    let selection = choices[Math.floor(Math.random()*3)]
+    return selection
+  
+  }
+
+  function game() {
+
+    for (let i = 0; rounds < 5; i++) {
+    playRound(playerSelection, computerSelection= getComputerChoice())
+    console.log(`Player: ${playerScore} Computer: ${computerScore} Draws: ${draws} Rounds: ${rounds}`)
+    
+
+
+    if (playerScore === 5) {
+        return `You win!`
+        } else if (computerScore === 5) {
+        return `You lose!`
+        } else if (draws === 5) {
+        return `Tie!`
+         }
+        
+        
+    }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
 
-let playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
-
-console.log(playerSelection);
-
-//Write a function that plays a single round of the game.//
-function playRound(){
-    if (playerSelection === computerSelection){
-        return alert("It's a tie! Try again.");
-    } else if (playerSelection == "rock" && computerSelection === "paper"){
-        return alert("Paper beats Rock. You lose :(");
-    } else if (playerSelection == "paper" && computerSelection === "rock"){
-        return alert("Rock beats Paper. You lose :(");
-    } else if (playerSelection == "scissors" && computerSelection === "rock"){
-        return alert("Rock beats Scissors. You lose :(");
-    } else if (playerSelection == "rock" && computerSelection === "scissors"){
-        return alert("Rock beats Scissors. You win!");
-    } else if (playerSelection == "paper" && computerSelection === "scissors"){
-        return alert("Scissors beat Paper. You lose :(");
-    } else if (playerSelection == "scissors" && computerSelection === "paper"){
-        return alert("Scissors beat Paper. You win!");
-    } else
-        return alert("you made a spelling mistake!!!");
+    if (playerSelection === `rock` && computerSelection === `paper`) {
+        return computerScore++ && prompt("Rock, Paper, or Scissors?", "")
+    } else if (playerSelection === `rock` && computerSelection === `scissors`) {
+        return playerScore++ && prompt("Rock, Paper, or Scissors?", "")
+    } else if (playerSelection === `paper` && computerSelection === `scissors`) {
+        return computerScore++ && prompt("Rock, Paper, or Scissors?", "")
+    } else if (playerSelection === `paper` && computerSelection === `rock`) {
+        return playerScore++ && prompt("Rock, Paper, or Scissors?", "")
+    } else if (playerSelection === `scissors` && computerSelection === `paper`) {
+        return playerScore++ && prompt("Rock, Paper, or Scissors?", "")
+    } else if (playerSelection === `scissors` && computerSelection === `rock`) {
+        return computerScore++ && prompt("Rock, Paper, or Scissors?", "")
+    } else if (playerSelection === computerSelection) {
+        return draws++ && prompt("Rock, Paper, or Scissors?", "")
+    }     
 }
-playRound();
+
+console.log(game())
+
+console.log(playRound(playerSelection, computerSelection));
